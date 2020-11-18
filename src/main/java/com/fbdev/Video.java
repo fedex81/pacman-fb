bbd1b04d7224e97079f38f4c438db7c20d2b2c37
Federico Berti <berti@leet.it>
2020-11-18 12:27:20 +0000

    tile layer, add top and bottom

--- src/main/java/com/fbdev/Video.java
+++ src/main/java/com/fbdev/Video.java
@@ -8,11 +8,10 @@ import com.fbdev.helios.model.SystemProvider;
 import com.fbdev.helios.util.VideoMode;
 import com.fbdev.ui.SwingWindow;
 import com.fbdev.util.RomHelper;
+import com.fbdev.util.VideoUtil;
 
 import java.awt.*;
 import java.util.Optional;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 /**
  * Federico Berti
@@ -21,28 +20,19 @@ import java.util.function.Function;
  */
 public class Video {
 
-    private static int[] colorBitWeight = {0x21, 0x47, 0x97, 0x21, 0x47, 0x97, 0x51, 0xAE};
-    private static BiFunction<Integer, Integer, Integer> cromBitDecodeFn = (d, b) -> (((d >> b) & 1) * colorBitWeight[b]);
-    private static Function<Integer, Integer> toRedFn = d -> cromBitDecodeFn.apply(d, 0) + cromBitDecodeFn.apply(d, 1) +
-            cromBitDecodeFn.apply(d, 2);
-    private static Function<Integer, Integer> toGreenFn = d -> cromBitDecodeFn.apply(d, 3) + cromBitDecodeFn.apply(d, 4) +
-            cromBitDecodeFn.apply(d, 5);
-    private static Function<Integer, Integer> toBlueFn = d -> cromBitDecodeFn.apply(d, 6) + cromBitDecodeFn.apply(d, 7);
-
-    private static final int tileW = VideoMode.H28_V32.getDimension().width / 8,
-            tileH = VideoMode.H28_V32.getDimension().height / 8;
+    private static final int tileW = VideoMode.H28_V36.getDimension().width / 8,
+            tileH = VideoMode.H28_V36.getDimension().height / 8;
     private static final int[] tileMapper = new int[tileW * tileH];
-    private byte[] crom, palrom, tileRom, spriteRom;
+    private final byte[] crom, palrom, tileRom, spriteRom;
+    private final byte[] ram;
+    private final Color[] colors;
+    private final int[][] paletteToColorsIdx = new int[64][4];
+    private final int[][] tileToPaletteIdx = new int[256][64];
+    private final int[][] spriteToPaletteIdx = new int[64][256];
 
-    private Color[] colors;
-    private int[][] paletteToColorsIdx = new int[64][4];
-    private int[][] tileToPaletteIdx = new int[256][64];
     int[] render = new int[tileW * 8 * tileH * 8];
-    boolean frameShown = false;
     SwingWindow window;
     JoypadProvider joypadProvider;
-    private int[][] spriteToPaletteIdx = new int[64][256];
-    private byte[] ram;
 
     public Video(RomHelper r, byte[] ram, JoypadProvider joypadProvider) {
         this.crom = r.getCrom();
@@ -55,68 +45,13 @@ public class Video {
         init();
     }
 
-    public static void generateTileMapper() {
-        int start = 0x3A0;
-        int rowIdx = start, colIdx = start;
-        for (int i = 0; i < tileMapper.length; i++) {
-//            System.out.printf("%4x ", rowIdx);
-            tileMapper[i] = rowIdx;
-            rowIdx -= 0x20;
-            if ((i + 1) % tileW == 0) {
-//                System.out.println();
-                colIdx++;
-                rowIdx = colIdx;
-            }
-        }
-
-    }
-
     private void init() {
         showFrame();
-        generateTileMapper();
-        for (int i = 0; i < crom.length; i++) {
-            int data = crom[i];
-            colors[i] = new Color(toRedFn.apply(data), toGreenFn.apply(data), toBlueFn.apply(data));
-        }
-        int k = 0;
-        for (int i = 0; i < palrom.length / 4; i++) {
-            paletteToColorsIdx[i][0] = palrom[k] & 0xF;
-            paletteToColorsIdx[i][1] = palrom[k + 1] & 0xF;
-            paletteToColorsIdx[i][2] = palrom[k + 2] & 0xF;
-            paletteToColorsIdx[i][3] = palrom[k + 3] & 0xF;
-            k += 4;
-        }
-        int[] num = {63, 31};
-        for (int i = 0; i < tileRom.length; i++) {
-            int quadrant = (i % 16) / 8; //an 8x4 area, each tile has 2
-            int numTile = i / 16;
-            int pos = num[quadrant] - (i % 8);
-            int val = tileRom[i];
-            int p1TileIdx = ((val >> 3) & 2) | (val & 1);
-            int p2TileIdx = ((val >> 4) & 2) | ((val >> 1) & 1);
-            int p3TileIdx = ((val >> 5) & 2) | ((val >> 2) & 1);
-            int p4TileIdx = ((val >> 6) & 2) | ((val >> 3) & 1);
-            tileToPaletteIdx[numTile][pos] = p1TileIdx;
-            tileToPaletteIdx[numTile][pos - 8] = p2TileIdx;
-            tileToPaletteIdx[numTile][pos - 16] = p3TileIdx;
-            tileToPaletteIdx[numTile][pos - 24] = p4TileIdx;
-        }
-        int[] numq = {255, 63, 127, 191, 247, 55, 119, 183};
-        for (int i = 0; i < spriteRom.length; i++) {
-            int numSprite = i / 64;
-            int quadrant = (i % 64) / 8; //an 8x4 area, each sprite has 8
-            int pos = numq[quadrant] - (i % 8);
-            int yShift = 16;
-            int val = spriteRom[i];
-            int p1TileIdx = ((val >> 3) & 2) | (val & 1);
-            int p2TileIdx = ((val >> 4) & 2) | ((val >> 1) & 1);
-            int p3TileIdx = ((val >> 5) & 2) | ((val >> 2) & 1);
-            int p4TileIdx = ((val >> 6) & 2) | ((val >> 3) & 1);
-            spriteToPaletteIdx[numSprite][pos] = p1TileIdx;
-            spriteToPaletteIdx[numSprite][pos - yShift] = p2TileIdx;
-            spriteToPaletteIdx[numSprite][pos - 2 * yShift] = p3TileIdx;
-            spriteToPaletteIdx[numSprite][pos - 3 * yShift] = p4TileIdx;
-        }
+        VideoUtil.generateTileMapper(tileMapper);
+        VideoUtil.generateColors(crom, colors);
+        VideoUtil.generatePaletteToCromIdx(palrom, paletteToColorsIdx);
+        VideoUtil.generateTileToPaletteIdx(tileRom, tileToPaletteIdx);
+        VideoUtil.generateSpriteToPaletteIdx(spriteRom, spriteToPaletteIdx);
     }
 
     public void composeImage() {
@@ -128,6 +63,9 @@ public class Video {
         int tileLineStartPx = 0;
         for (int i = 0; i < tilesToProcess; i++) {
             int tileLoc = tileMapper[i];
+            if (tileLoc < 0) {
+                continue;
+            }
             int tileIdx = ram[tileLoc] & 0xFF;
             int[] paletteIndexes = tileToPaletteIdx[tileIdx]; //64 pixel, a palette index for each
             int paletteIdx = ram[tileLoc + SystemBus.PALETTE_RAM_OFFSET] & 0x3F; // 64 palettes
@@ -148,7 +86,7 @@ public class Video {
                 startAddrPx += 8;
             }
         }
-        window.renderScreenLinear(render, Optional.empty(), VideoMode.H28_V32);
+        window.renderScreenLinear(render, Optional.empty(), VideoMode.H28_V36);
     }
 
     private void showFrame() {
---
+++ src/main/java/com/fbdev/util/VideoUtil.java
@@ -0,0 +1,132 @@
+package com.fbdev.util;
+
+import com.fbdev.helios.util.VideoMode;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * Federico Berti
+ * <p>
+ * Copyright 2020
+ */
+public class VideoUtil {
+
+    private static final int tileW = VideoMode.H28_V36.getDimension().width / 8,
+            tileH = VideoMode.H28_V36.getDimension().height / 8;
+
+    private static int[] colorBitWeight = {0x21, 0x47, 0x97, 0x21, 0x47, 0x97, 0x51, 0xAE};
+    private static BiFunction<Integer, Integer, Integer> cromBitDecodeFn = (d, b) -> (((d >> b) & 1) * colorBitWeight[b]);
+    private static Function<Integer, Integer> toRedFn = d -> cromBitDecodeFn.apply(d, 0) + cromBitDecodeFn.apply(d, 1) +
+            cromBitDecodeFn.apply(d, 2);
+    private static Function<Integer, Integer> toGreenFn = d -> cromBitDecodeFn.apply(d, 3) + cromBitDecodeFn.apply(d, 4) +
+            cromBitDecodeFn.apply(d, 5);
+    private static Function<Integer, Integer> toBlueFn = d -> cromBitDecodeFn.apply(d, 6) + cromBitDecodeFn.apply(d, 7);
+
+    //well this is ugly
+    public static void generateTileMapper(int[] tileMapper) {
+        Arrays.fill(tileMapper, -1);
+        //top tiles 2*36, skip 8
+        int start = 0x3DD;
+        int rowIdx = start, colIdx;
+        int idx = 2;
+        for (; idx < tileW * 2 + 2; idx++) {
+//            System.out.printf("%4x ", rowIdx);
+            tileMapper[idx] = rowIdx;
+            rowIdx--;
+            if (idx == tileW + 2 - 1) {
+//                System.out.println();
+                rowIdx = 0x3FD;
+            }
+        }
+//        System.out.println();
+        //center tiles 28*32
+        start = 0x3A0;
+        idx -= 2;
+        rowIdx = colIdx = start;
+        int len = idx + tileW * 32;
+        for (; idx < len; idx++) {
+//            System.out.printf("%4x ", rowIdx);
+            tileMapper[idx] = rowIdx;
+            rowIdx -= 0x20;
+            if ((idx + 1) % tileW == 0) {
+//                System.out.println();
+                colIdx++;
+                rowIdx = colIdx;
+            }
+        }
+//        System.out.println();
+        //bottom tiles 2*36, skip 8
+        start = 0x01D;
+        rowIdx = start;
+//        idx += 4;
+        len = idx + tileW * 2;
+        for (; idx < len; idx++) {
+//            System.out.printf("%4x ", rowIdx);
+            tileMapper[idx] = rowIdx;
+            rowIdx--;
+            if ((idx + 1) % tileW == 0) {
+//                System.out.println();
+                rowIdx = 0x03D;
+            }
+        }
+//        System.out.println();
+    }
+
+    public static void generateColors(byte[] crom, Color[] colors) {
+        for (int i = 0; i < crom.length; i++) {
+            int data = crom[i];
+            colors[i] = new Color(toRedFn.apply(data), toGreenFn.apply(data), toBlueFn.apply(data));
+        }
+    }
+
+    public static void generatePaletteToCromIdx(byte[] palrom, int[][] paletteToColorsIdx) {
+        int k = 0;
+        for (int i = 0; i < palrom.length / 4; i++) {
+            paletteToColorsIdx[i][0] = palrom[k] & 0xF;
+            paletteToColorsIdx[i][1] = palrom[k + 1] & 0xF;
+            paletteToColorsIdx[i][2] = palrom[k + 2] & 0xF;
+            paletteToColorsIdx[i][3] = palrom[k + 3] & 0xF;
+            k += 4;
+        }
+    }
+
+    public static void generateTileToPaletteIdx(byte[] tileRom, int[][] tileToPaletteIdx) {
+        int[] num = {63, 31};
+        for (int i = 0; i < tileRom.length; i++) {
+            int quadrant = (i % 16) / 8; //an 8x4 area, each tile has 2
+            int numTile = i / 16;
+            int pos = num[quadrant] - (i % 8);
+            int val = tileRom[i];
+            int p1TileIdx = ((val >> 3) & 2) | (val & 1);
+            int p2TileIdx = ((val >> 4) & 2) | ((val >> 1) & 1);
+            int p3TileIdx = ((val >> 5) & 2) | ((val >> 2) & 1);
+            int p4TileIdx = ((val >> 6) & 2) | ((val >> 3) & 1);
+            tileToPaletteIdx[numTile][pos] = p1TileIdx;
+            tileToPaletteIdx[numTile][pos - 8] = p2TileIdx;
+            tileToPaletteIdx[numTile][pos - 16] = p3TileIdx;
+            tileToPaletteIdx[numTile][pos - 24] = p4TileIdx;
+        }
+    }
+
+    public static void generateSpriteToPaletteIdx(byte[] spriteRom, int[][] spriteToPaletteIdx) {
+        int[] numq = {255, 63, 127, 191, 247, 55, 119, 183};
+        for (int i = 0; i < spriteRom.length; i++) {
+            int numSprite = i / 64;
+            int quadrant = (i % 64) / 8; //an 8x4 area, each sprite has 8
+            int pos = numq[quadrant] - (i % 8);
+            int yShift = 16;
+            int val = spriteRom[i];
+            int p1TileIdx = ((val >> 3) & 2) | (val & 1);
+            int p2TileIdx = ((val >> 4) & 2) | ((val >> 1) & 1);
+            int p3TileIdx = ((val >> 5) & 2) | ((val >> 2) & 1);
+            int p4TileIdx = ((val >> 6) & 2) | ((val >> 3) & 1);
+            spriteToPaletteIdx[numSprite][pos] = p1TileIdx;
+            spriteToPaletteIdx[numSprite][pos - yShift] = p2TileIdx;
+            spriteToPaletteIdx[numSprite][pos - 2 * yShift] = p3TileIdx;
+            spriteToPaletteIdx[numSprite][pos - 3 * yShift] = p4TileIdx;
+        }
+    }
+}
