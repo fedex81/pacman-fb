85b1a3a96ecac6a61c9cbd5e823b01f60f2dae20
Federico Berti <berti@leet.it>
2020-11-21 23:29:19 +0000

    copyright

--- src/main/java/com/fbdev/ui/debug/BaseViewer.java
+++ src/main/java/com/fbdev/ui/debug/BaseViewer.java
@@ -1,3 +1,22 @@
+/*
+ * BaseViewer
+ * Copyright (c) 2020-2020 Federico Berti
+ * Last modified: 20/11/2020, 13:39
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.fbdev.ui.debug;
 
 /**
@@ -60,8 +79,8 @@ public abstract class BaseViewer {
             label.setForeground(Color.BLACK);
             panel.add(label);
         }
-            int k = 0;
-            int rowCnt = 0;
+        int k = 0;
+        int rowCnt = 0;
         for (int i = 0; i < gridContext.entries; i++) {
             if (k % labelPerLine == 0) {
                 JLabel label = new JLabel(Integer.toHexString(rowCnt));
